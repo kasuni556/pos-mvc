875f13650b116bbaac04d06c531561fd7aca5a3f
kasuni556 <kasunikavindhya@gmail.com>
2023-08-15 17:11:56 +0530

    add more

--- pos-mvc/src/pos/mvc/PosMvc.java
+++ pos-mvc/src/pos/mvc/PosMvc.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pos.mvc;
-
+import pos.mvc.view.CustomerView;
 /**
  *
  * @author KASUNI
@@ -14,7 +14,7 @@ public class PosMvc {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new CustomerView().setVisible(true);
     }
     
 }
